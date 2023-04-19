c1daf7d73919e57a1d0d45ee663710942e0a3a79
moosimoosihan <segun408@gmail.com>
2023-04-19 16:40:24 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import {useEffect, useState} from "react";
 
+function Hello(){
+
+  function byFn(){
+    console.log("bye :(");
+  }
+  function hiFn(){
+    console.log("created :)");
+    return byFn;
+  }
+  
+  useEffect(()=>{
+    console.log("hi :)");
+    return function (){
+      console.log("bye :(");
+    }
+  },[]);
+  
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(()=>{
-    console.log("i run only once.");
-  }, []);
-  useEffect(()=>{
-      console.log("i run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(()=>{
-    console.log("i run when 'counter' changes");
-  }, [counter]);
-  useEffect(()=>{
-    console.log("i run when 'keyword & counter' changes");
-  }, [keyword ,counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev)=>!prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1 className={styles.title}>{counter}</h1>
-      <Button text={"counter"} onClick={onClick} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing?"Hide":"Show"}</button>
     </div>
   );
 }
