e5d564effcfb990f837f6936eb84de86df0aa58d
moosimoosihan <segun408@gmail.com>
2023-04-20 10:42:20 +0900

    coin

--- src/App.js
+++ src/App.js
@@ -3,29 +3,83 @@ import styles from "./App.module.css";
 import {useEffect, useState} from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo===""){
-      return;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+  const [amount, setAmount] = useState(0);
+
+  function onChange(event) {
+    setAmount(event.target.value);
+  }
+
+  function reset() {
+    setAmount(0);
+  }
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedCoin && amount) {
+      const price = parseFloat(selectedCoin.quotes.USD.price);
+      const btcAmount = (amount / price).toFixed(8);
+      document.getElementById("BTC").value = btcAmount;
+    } else {
+      document.getElementById("BTC").value = "";
     }
-    setToDos(currentArray => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  }, [selectedCoin, amount]);
+
+  function onSelectCoin(event) {
+    const coinId = event.target.value;
+    const coin = coins.find((coin) => coin.id === coinId);
+    setSelectedCoin(coin);
+  }
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} text="text" placeholder="Write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index)=><li key={index}>{item}</li>)}
-      </ul>
-      
+      <h1>
+        The Coins! {loading ? "" : `(${coins.length})`}
+      </h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelectCoin}>
+          <option value="">Select a coin</option>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.id}>
+              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <div>
+        <div>
+          <label htmlFor="USD">USD</label>
+          <input
+            value={amount}
+            id="USD"
+            placeholder="USD"
+            type="number"
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="BTC">BTC</label>
+          <input
+            id="BTC"
+            placeholder="BTC"
+            type="number"
+            disabled={true}
+          />
+        </div>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
